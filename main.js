97dd3d4a3701acf8d2992599740704b8325540dc
slahbk <slah.eddine.ben.kacem@gmail.com>
2023-12-16 11:23:54 +0100

    put the title bike shop between anchor

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ function generateID() {
       var values = Object.values(ele1)
         each(values, function(ele2, i){ // to search for string
           if(typeof(ele2) == 'string'){
-            var words = ele2.split(' ') // maybe the string have two word so we split it
+            var words = ele2.split(' ') // maybe the string have more than one word so we split it
             each(words, function(word,i){
                 if(word.toLowerCase() == categoryOrName.toLowerCase()){ // lowercase the input and the string in the list to avoid problem
                     result.push(ele1)
@@ -101,7 +101,7 @@ function generateID() {
 
   // Add products to the shop
   bikeShop.add('SANTA CRUZ V10', 6799, 'Bikes', ['https://www.santacruzbicycles.com/_next/image?url=https%3A%2F%2Fimages.prismic.io%2Fsantacruzbikesstatic%2Fa1e5f9b6-5c53-4d2c-9df0-791e235a196a_V10-bottom_Desktop.png%3Fauto%3Dcompress%2Cformat&w=2100&q=99',' https://www.santacruzbicycles.com/_next/image?url=https%3A%2F%2Fimages.prismic.io%2Fsantacruzbikesstatic%2F8a1e50a3-fd74-412c-9d8f-30165c590a80_MY24_V10_8_profile_desktop.jpg%3Fauto%3Dcompress%2Cformat&w=2100&q=99','https://www.santacruzbicycles.com/_next/image?url=https%3A%2F%2Fimages.prismic.io%2Fsantacruzbikesstatic%2F16fa60d5-8002-4091-825e-d74fa5f0e0db_Rear.png%3Fauto%3Dcompress%252Cformat%26fit%3Dcrop%26ar%3D9%253A7%26crop%3Dfaces%252Cedges&w=1200&q=75'])
-  bikeShop.add('CANYON Speedmax CFR Disc Di2', 11999,'Bikes',['https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dw835b17b9/images/full/full_2022_/2022/full_2022_speedmax-cfr-disc-di2_2921_bu-bk_P5.jpg?sw=1300&sfrm=png&q=90&bgcolor=F2F2F2','https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dw3d17cccb/images/top/2921_TOP-1/2022/2921_TOP-1_speedmax-cfr-disc-di2_bu-bk_cockpit.jpg?sw=1300&sh=1300&sm=cut&sfrm=jpg&q=80','https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dwa9b38eb8/images/top/2921_TOP-4/2022/2921_TOP-4_speedmax-cfr-disc-di2_bu-bk_fork-disc.jpg?sw=1300&sh=1300&sm=cut&sfrm=jpg&q=80'])
+  bikeShop.add('CANYON Speedmax CFR Disc Di2', 11999,'Bikes',['https://file.removal.ai/preview/be93360a-b936-4f43-8bc7-3b17a2e57418-full_2022_speedmax-cfr-disc-di2_2921_bu-bk_p5.png','https://file.removal.ai/preview/7c0602a6-b707-4d25-9f17-0857cda4d512-2921_top-1_speedmax-cfr-disc-di2_bu-bk_cockpit.png','https://www.canyon.com/dw/image/v2/BCML_PRD/on/demandware.static/-/Sites-canyon-master/default/dwa9b38eb8/images/top/2921_TOP-4/2022/2921_TOP-4_speedmax-cfr-disc-di2_bu-bk_fork-disc.jpg?sw=1300&sh=1300&sm=cut&sfrm=jpg&q=80'])
   bikeShop.add('TREK Session 9 X01', 7199, 'Bikes', ['https://media.trekbikes.com/image/upload/f_auto,fl_progressive:semi,q_auto,w_1024,h_768,c_pad/Session9X01-24-41823-A-Portrait','https://media.trekbikes.com/image/upload/f_auto,fl_progressive:semi,q_auto,w_1024,h_768,c_pad/Session9X01-24-41823-A-Primary','https://p.vitalmtb.com/styles/full_size_1600/s3/photos/products/27491/photos/1782946/session_03.jpg?VersionId=3LARoXnYwYRasmJeIve36ZiwU0E4j0lh&itok=RohuFkje'])
   bikeShop.add("helmet Giro Aries Spherical", 299, 'accessories', ['https://m.media-amazon.com/images/I/61p5BUlV4sL._AC_SL1500_.jpg','https://m.media-amazon.com/images/I/61ttbjXAaQL._AC_SL1500_.jpg'])
   bikeShop.add("Water Bottle CamelBak Podium Dirt", 12.99, 'accessories', ['https://m.media-amazon.com/images/I/41BEL5b+xuL._AC_SL1000_.jpg','https://m.media-amazon.com/images/I/41rxqSDsCiL._AC_SL1000_.jpg','https://m.media-amazon.com/images/I/51FFOD1QeVL._AC_SL1500_.jpg'])
@@ -110,7 +110,7 @@ function generateID() {
   function displayAll(array){ // display all in the shop list
     $('#shopPage').empty() // we do that to avoid repeatition 
     if(array.length === 0){
-      $('#shopPage').append(`<h1 id='sorry'>${'Sorry!! No Products Found: ' + $('#searchInput').val()}</h1>`)
+      $('#shopPage').append(`<h1 id='sorry'>${'Sorry!! No Products Found: ' + $('#searchInput').val()}!!</h1>`)
     }
     each(array, function(ele, i){
       $('#shopPage').append(`
@@ -147,7 +147,6 @@ function generateID() {
     }
     $('#searching').click(displayBy)
     
-}
-
+  }
 // Display all products initially
 displayAll(bikeShop.list)
